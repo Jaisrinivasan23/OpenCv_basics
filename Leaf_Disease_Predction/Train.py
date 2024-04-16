14ee2360623ef3dfb5bc402d9e7b476351ed204e
jaiisrinivasan23 <jaiisrinivasan2305@gmail.com>
2024-04-16 17:59:07 +0530

    Added Text_Recg.py

---
+++ Leaf_Disease_Predction/Train.py
@@ -0,0 +1,45 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense, BatchNormalization
+from keras.preprocessing.image import ImageDataGenerator
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(38, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',metrics=['accuracy'],optimizer='adam')
+
+train_datagen = ImageDataGenerator(rescale=None,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory('data/train')
---
+++ Text_Recg.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+
+def recText(filename):
+    text = pytesseract.image_to_string(Image.open(filename))
+    return text
+
+info = recText('Datasets\Screenshot 2024-04-15 184535.png')
+print(info)
